f483878cde0fc3b40deff2b4a86372f127a0951f
seanhardison1 <seanhardison@gmail.com>
2019-09-09 16:48:40 -0400

    Changing values of knots

--- R/gam_example.R
+++ R/gam_example.R
@@ -1,5 +1,6 @@
 library(mgcv)
 library(tidyverse)
+library(cowplot)
 
 mcycle <- MASS::mcycle
 
@@ -7,21 +8,28 @@ mcycle <- MASS::mcycle
 ggplot(data = mcycle, aes(x = times, y = accel)) +
   geom_point()
 
-mod <- gam(accel ~ s(times), data = mcycle)
 
 
-pred <- predict(mod, se.fit = TRUE)
+plot_mod <- function(df, k){
+  mod <- gam(accel ~ s(times, k = k), data = df)
+  pred <- predict(mod, se.fit = TRUE)
+  
+  pred_df <- data.frame(prediction = pred$fit,
+                        x = mcycle$times,
+                        upCI = pred$fit + pred$se.fit * 2,
+                        lwCI = pred$fit - pred$se.fit * 2)
+  
+  ggplot() +
+    geom_point(data = mcycle, aes(x = times, y = accel)) +
+    geom_line(data = pred_df, aes(x = x, y = prediction)) +
+    geom_ribbon(data = pred_df, aes(x = x, ymin = lwCI, 
+                                    ymax = upCI), alpha = 0.2, fill = "blue")
+}
 
-pred_df <- data.frame(prediction = pred$fit,
-                      x = mcycle$times,
-                      upCI = pred$fit + pred$se.fit * 2,
-                      lwCI = pred$fit - pred$se.fit * 2)
+k1 <- plot_mod(df = mcycle, k = 1)
+k2 <- plot_mod(df = mcycle, k = 5)
+k10 <- plot_mod(df = mcycle, k = 10)
+k20 <- plot_mod(df = mcycle, k = 20)
 
-ggplot() +
-  geom_point(data = mcycle, aes(x = times, y = accel)) +
-  geom_line(data = pred_df, aes(x = x, y = prediction)) +
-  geom_ribbon(data = pred_df, aes(x = x, ymin = lwCI, 
-                  ymax = upCI), alpha = 0.2, fill = "blue")
-
-
+cowplot::plot_grid(k1, k2, k10, k20)
 
